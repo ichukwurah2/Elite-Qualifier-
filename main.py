2a7176249537f3da7f2b66a8ce0b6a4430dd19b6
student Ifeanyichukwu Ifeanyichukwu <s492275@student.roundrockisd.org>
2021-02-24 05:47:15 +0000

    Adding Python Google Searcher 02/23/21

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 ​import random
+import requests, sys, webbrowser, bs4
 import time
+
+res = requests.get('http://google.com/search?q='+'.join(sys.argv[1:]))
+res.raise_for_status()
+
+soup = bs4.BeatifulSoup(res.text, "html.parser")
+LinkElements = soup.Select('.r a')
+LinkToOpen = min(5, len(LinkElements))
+for i in range(linkToOpen):
+    webbrowser.open('http://google.com'+LinkElements)
 
 name = input("Hello, what is your name? ")
 
